171e9dd05bcb6fe59cc3d21cc4cdb6fc8bb08ad1
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-06 14:44:54 +0700

    import image to data

--- src/data.js
+++ src/data.js
@@ -1,75 +1,88 @@
+import NikePhantom from "./Assets/data/nike-phantom.jpg";
+import AirJordan from "./Assets/data/Nike Air Jordan.jpg";
+import RolexSpiral from "./Assets/data/Rolex Spiral.jpg";
+import RolexMadu from "./Assets/data/Rolex Madu.jpg";
+import DasiPremium from "./Assets/data/Dasi Premium.jpg";
+import ShirtBlack from "./Assets/data/T Shirt Black.jpg";
+import ShirtGreen from "./Assets/data/T Shirt Green.jpg";
+import DaxaPro from "./Assets/data/Daxa M71 Pro.jpg";
+import KeychornBlack from "./Assets/data/Keychorn Black.jpg";
+import KeychorWhite from "./Assets/data/Keychorn White.jpg";
+import SamsungWtach from "./Assets/data/Samsung Watch.jpg";
+import AppleKeyboard from "./Assets/data/Apple Keyboard.jpg";
+
 const dataProduct = [
   {
     id: 1,
     nama: "Sepatu Nike phantom",
     harga: 100000,
-    image: require("./Assets/data/nike-phantom.jpg"),
+    image: NikePhantom,
   },
   {
     id: 2,
     nama: "Sepatu Nike Air Jordan",
     harga: 12000000,
-    image: require("./Assets/data/Nike Air Jordan.jpg"),
+    image: AirJordan,
   },
   {
     id: 3,
     nama: "Rolex Spiral",
     harga: 90000000,
-    image: require("./Assets/data/Rolex Spiral.jpg"),
+    image: RolexSpiral,
   },
   {
     id: 4,
     nama: "Rolex Madu",
     harga: 1200000,
-    image: require("./Assets/data/Rolex Madu.jpg"),
+    image: RolexMadu,
   },
   {
     id: 5,
     nama: "Dasi Premium",
     harga: 100000,
-    image: require("./Assets/data/Dasi Premium.jpg"),
+    image: DasiPremium,
   },
   {
     id: 6,
     nama: "T Shirt Black",
     harga: 100000,
-    image: require("./Assets/data/T Shirt Black.jpg"),
+    image: ShirtBlack,
   },
   {
     id: 7,
     nama: "T Shirt Green",
     harga: 100000,
-    image: require("./Assets/data/T Shirt Green.jpg"),
+    image: ShirtGreen,
   },
   {
     id: 8,
     nama: "Daxa M71 Pro",
     harga: 900000,
-    image: require("./Assets/data/Daxa M71 Pro.jpg"),
+    image: DaxaPro,
   },
   {
     id: 9,
     nama: "Keychorn Black",
     harga: 1000000,
-    image: require("./Assets/data/Keychorn Black.jpg"),
+    image: KeychornBlack,
   },
   {
     id: 10,
     nama: "Keychorn White",
     harga: 1000000,
-    image: require("./Assets/data/Keychorn White.jpg"),
+    image: KeychorWhite,
   },
   {
     id: 11,
     nama: "Samsung Watch",
     harga: 1200000,
-    image: require("./Assets/data/Samsung Watch.jpg"),
+    image: SamsungWtach,
   },
   {
     id: 12,
     nama: "Apple Keyboard",
     harga: 1000000,
-    image: require("./Assets/data/Apple Keyboard.jpg"),
+    image: AppleKeyboard,
   },
 ];
 
