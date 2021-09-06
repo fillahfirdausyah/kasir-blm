8434697a025c3d6220da2b6de6a2f01f33dbd46e
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-06 11:46:33 +0700

    make data dummy

---
+++ src/data.js
@@ -0,0 +1,74 @@
+const dataProduct = [
+  {
+    id: 1,
+    nama: "Sepatu Nike phantom",
+    harga: 100000,
+    image: require("./Assets/data/nike-phantom.jpg"),
+  },
+  {
+    id: 2,
+    nama: "Sepatu Nike Air Jordan",
+    harga: 12000000,
+    image: require("./Assets/data/Nike Air Jordan.jpg"),
+  },
+  {
+    id: 3,
+    nama: "Rolex Spiral",
+    harga: 90000000,
+    image: require("./Assets/data/Rolex Spiral.jpg"),
+  },
+  {
+    id: 4,
+    nama: "Rolex Madu",
+    harga: 1200000,
+    image: require("./Assets/data/Rolex Madu.jpg"),
+  },
+  {
+    id: 5,
+    nama: "Dasi Premium",
+    harga: 100000,
+    image: require("./Assets/data/Dasi Premium.jpg"),
+  },
+  {
+    id: 6,
+    nama: "T Shirt Black",
+    harga: 100000,
+    image: require("./Assets/data/T Shirt Black.jpg"),
+  },
+  {
+    id: 7,
+    nama: "T Shirt Green",
+    harga: 100000,
+    image: require("./Assets/data/T Shirt Green.jpg"),
+  },
+  {
+    id: 8,
+    nama: "Daxa M71 Pro",
+    harga: 900000,
+    image: require("./Assets/data/Daxa M71 Pro.jpg"),
+  },
+  {
+    id: 9,
+    nama: "Keychorn Black",
+    harga: 1000000,
+    image: require("./Assets/data/Keychorn Black.jpg"),
+  },
+  {
+    id: 10,
+    nama: "Keychorn White",
+    harga: 1000000,
+    image: require("./Assets/data/Keychorn White.jpg"),
+  },
+  {
+    id: 11,
+    nama: "Samsung Watch",
+    harga: 1200000,
+    image: require("./Assets/data/Samsung Watch.jpg"),
+  },
+  {
+    id: 12,
+    nama: "Apple Keyboard",
+    harga: 1000000,
+    image: require("./Assets/data/Apple Keyboard.jpg"),
+  },
+];
