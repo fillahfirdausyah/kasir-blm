e8d2fc18db5d450d4e63cbccb08e66c6f4db4ec5
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-06 13:35:08 +0700

    fixed style for side menu component

--- src/Components/SideMenu/index.js
+++ src/Components/SideMenu/index.js
@@ -10,20 +10,30 @@ function SideMenu() {
       </header>
       <div className="__sideMenu_body">
         <div className="__sideMenu_body_productName">
-          <h4>Nama Produk</h4>
-          <p>Nike Phantom</p>
+          <h4>Nike Phantom</h4>
+          <p>200,000</p>
         </div>
         <div className="__sideMenu_body_productName">
-          <h4>Harga Produk</h4>
-          <p>Rp 100,000</p>
+          <h4>Nike Phantom</h4>
+          <p>200,000</p>
         </div>
         <div className="__sideMenu_body_productName">
-          <h4>Qty</h4>
-          <p>1x</p>
+          <h4>Nike Phantom</h4>
+          <p>200,000</p>
         </div>
+        <div className="__sideMenu_body_productName">
+          <h4>Nike Phantom</h4>
+          <p>200,000</p>
+        </div>
+        <div className="__sideMenu_body_productName">
+          <h4>Nike Phantom</h4>
+          <p>200,000</p>
+        </div>
+      </div>
+      <div className="__totalWrapper">
         <div className="__sideMenu_body_total">
           <h4>Total</h4>
-          <p>100,000</p>
+          <p>1,000,000</p>
         </div>
         <div className="__sideMenu_button">Proses</div>
       </div>
