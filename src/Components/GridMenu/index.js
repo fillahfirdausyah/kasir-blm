75a2065ab5bcbb48b1694161339c0be0b4cf88f7
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-08 14:25:31 +0700

    get dummy data and pas to side menu component

--- src/Components/GridMenu/index.js
+++ src/Components/GridMenu/index.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
-import dataProduct from "../../data";
-
 // Component
-import SearchIcon from "@material-ui/icons/Search";
 
 function GridMenu({ getItems }) {
+  const [data, setData] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [collectCart, setCollectCart] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=10")
+      .then((res) => res.json())
+      .then((res) => setData(res));
+    fetch("https://fakestoreapi.com/products/categories")
+      .then((res) => res.json())
+      .then((res) => setCategory(res));
+  }, []);
+
+  useEffect(() => {
+    getItems(collectCart);
+  }, [collectCart]);
+
+  const addItem = (data) => {
+    setCollectCart([...collectCart, data]);
+  };
+
   return (
     <div className="__gridMenuWrapper">
       <div className="__gridMenuCategory">
         <h3>Category</h3>
         <div className="__gridMenuCategoryBody">
-          <div className="__gridMenuCategoryItem">
+          {/* <div className="__gridMenuCategoryItem">
             <div className="__gridMenuCategoryImageWrapper">
               <img
                 src="https://n4.sdlcdn.com/imgs/b/n/e/Being-Fab-Green-Formal-Shirt-SDL798545728-1-0b951.jpg"
@@ -29,71 +47,43 @@ function GridMenu({ getItems }) {
               />
             </div>
             <p>Jeans</p>
+          </div> */}
+          <div className="__categoryItemWrapper">
+            <ul>
+              {category.map((x, index) => (
+                <li key={index} className={index == 0 ? "active" : ""}>
+                  {x}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
       <div className="__gridMenu_body">
         <div className="__gridMenuHeader">
           <h3>Shirt</h3>
-          <p>37 Result</p>
+          <p>37 Hasil</p>
         </div>
         <div className="row">
-          <div className="col-6 col-md-4 col-xl-3">
-            <div className="__gridMenuItem">
-              <div className="__gridMenuImage">
-                <img
-                  src="https://www.kustomkit.com/colours/1200/KK710lightblue_front.jpg"
-                  alt=""
-                />
-              </div>
-              <div className="__gridMenuItemDetails">
-                <p>Kustom Kit</p>
-                <p>Rp 200,000</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-6 col-md-4 col-xl-3">
-            <div className="__gridMenuItem">
-              <div className="__gridMenuImage">
-                <img
-                  src="https://www.kustomkit.com/colours/1200/KK710lightblue_front.jpg"
-                  alt=""
-                />
-              </div>
-              <div className="__gridMenuItemDetails">
-                <p>Kustom Kit</p>
-                <p>Rp 200,000</p>
+          {data.map((x, index) => (
+            <div
+              className="col-6 col-md-4 col-xl-3"
+              data-aos="fade-right"
+              data-aos-duration="700"
+              key={index}
+              onClick={() => addItem(x)}
+            >
+              <div className="__gridMenuItem">
+                <div className="__gridMenuImage">
+                  <img src={x.image} alt="" />
+                </div>
+                <div className="__gridMenuItemDetails">
+                  <p>{x.title}</p>
+                  <p>${x.price}</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-6 col-md-4 col-xl-3">
-            <div className="__gridMenuItem">
-              <div className="__gridMenuImage">
-                <img
-                  src="https://www.kustomkit.com/colours/1200/KK710lightblue_front.jpg"
-                  alt=""
-                />
-              </div>
-              <div className="__gridMenuItemDetails">
-                <p>Kustom Kit</p>
-                <p>Rp 200,000</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-6 col-md-4 col-xl-3">
-            <div className="__gridMenuItem">
-              <div className="__gridMenuImage">
-                <img
-                  src="https://www.kustomkit.com/colours/1200/KK710lightblue_front.jpg"
-                  alt=""
-                />
-              </div>
-              <div className="__gridMenuItemDetails">
-                <p>Kustom Kit</p>
-                <p>Rp 200,000</p>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
