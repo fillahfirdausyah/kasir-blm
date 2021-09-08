37376fe386f598a52d6863e14ea78e3732cb1352
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-09-08 14:24:38 +0700

    ini aos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import HomePage from "./Pages/Homepage";
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  });
+
   return (
     <div className="App">
       <HomePage />
